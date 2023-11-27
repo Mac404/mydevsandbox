b8e75dc3990f7dbb48fef0bab824d4c80062f6be
Mac404 <macsf1@gmail.com>
2023-11-27 13:38:53 -0800

    Decorating the file

--- myfirstscript.sh
+++ myfirstscript.sh
@@ -1,6 +1,26 @@
 #this is a test
+
+echo "Installing Apache2"
+echo "--------------------------------------"
 sudo apt install apache2 -y
+echo "--------------------------------------"
+
+echo "Installing MySQL"
+echo "--------------------------------------"
 sudo apt install mysql-server -y
+echo "--------------------------------------"
+
+echo "Installing PostGre"
+echo "--------------------------------------"
 sudo apt install postgresql-server-dev-all -y
+echo "--------------------------------------"
+
+echo "Installing PostGre"
+echo "--------------------------------------"
 sudo apt install gnupg -y
+echo "--------------------------------------"
+
+echo "Installing Curl"
+echo "--------------------------------------"
 sudo apt install curl -y
+echo "--------------------------------------"
